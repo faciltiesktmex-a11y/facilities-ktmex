7bc2e29122bb1bf1a02ec19b3c5adf4b91802b95
faciltiesktmex-a11y <faciltiesktmex@gmail.com>
2026-09-11 03:07:41 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ktmex-facilities-v8-5a-20260911';
+const CACHE='ktmex-facilities-v8-5a1-20260911';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(c=>Promise.all(APP_SHELL.map(u=>c.add(u).catch(()=>null)))).then(()=>self.skipWaiting()));
